74786c88c4d33cc81ce6fef56b7912e47165b251
Anton Surkis <antonsurkis@gmail.com>
2019-03-23 22:45:28 +0300

    Added field to collection element

--- src/CollectionElement.java
+++ src/CollectionElement.java
@@ -1,5 +1,21 @@
 import java.io.Serializable;
 
 public class CollectionElement implements Serializable {
+    private String someString;
 
+    public CollectionElement(String someString) {
+        this.someString = someString;
+    }
+
+    public CollectionElement() {
+        this("");
+    }
+
+    public String getSomeString() {
+        return someString;
+    }
+
+    public void setSomeString(String someString) {
+        this.someString = someString;
+    }
 }
